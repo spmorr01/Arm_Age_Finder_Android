74131d20a9537d70a1028ea0e16e6a78e2904a91
Spencer Morris <spencermorris22@gmail.com>
2015-08-13 15:39:32 -0500

    Layout for MainActivity is finished for now. Added listeners for the checkboxes, where they're ready to then open their respective activities.

---
+++ app/src/main/java/com/armagefinder/morriss/armagefinder/PitcherActivity.java
@@ -0,0 +1,45 @@
+package com.armagefinder.morriss.armagefinder;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class PitcherActivity extends ActionBarActivity {
+    public TextView test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pitcher);
+        //test = (TextView)findViewById(R.id.test);
+        //String Age = Integer.toString(MainActivity.age);
+        //test.setText(Age);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_pitcher, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
