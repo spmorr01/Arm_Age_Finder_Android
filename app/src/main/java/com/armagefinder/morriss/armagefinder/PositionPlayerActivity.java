2691603d9d67b7bfcc5cf14fd00f33295bef21a4
Spencer Morris <spencermorris22@gmail.com>
2015-08-26 22:29:54 -0500

    Made some layout changes and took out settings button on labels.

--- app/src/main/java/com/armagefinder/morriss/armagefinder/PositionPlayerActivity.java
+++ app/src/main/java/com/armagefinder/morriss/armagefinder/PositionPlayerActivity.java
@@ -3,10 +3,8 @@ package com.armagefinder.morriss.armagefinder;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
@@ -468,27 +466,5 @@ public class PositionPlayerActivity extends AppCompatActivity {
             falseChecks++;
             return false;
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_position_player, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
